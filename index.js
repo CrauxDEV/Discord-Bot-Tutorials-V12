7656708da5b9086e65d71d1a21b91f5546c00882
CrauxDEV <83064873+CrauxDEV@users.noreply.github.com>
2021-05-26 06:17:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const discord = require('discord.js');
 const client = new discord.Client();
+const token = process.env.TOKEN;
+const prefix = process.env.PREFIX;
 
+client.commands 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
